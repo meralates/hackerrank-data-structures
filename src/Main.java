fb91bcb42cf3ff051f709a56f227a6af1e0fd073
Meral ATEŞ <202103001075@dogus.edu.tr>
2024-11-19 01:07:10 +0300

    java map

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        in.nextLine(); //satir sonunu atlar
+
+        Map<String,String> phoneBook = new HashMap<>();//phone book
+
+        //giris ekle
+        for (int i = 0; i < n; i++) {
+            String name = in.nextLine();
+            String phone = in.nextLine();
+            phoneBook.put(name, phone);
+        }
+        while (in.hasNext()) {
+            String s = in.nextLine();
+            if (phoneBook.containsKey(s)) {
+                System.out.println(s +" " + phoneBook.get(s));
+            }else{
+                System.out.println("not found");
+            }
+        }
+        in.close();
     }
 }
