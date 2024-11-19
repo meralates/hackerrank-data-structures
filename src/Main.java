4867134baf576d98b078808aac252d5b9af13151
Meral ATEŞ <202103001075@dogus.edu.tr>
2024-11-20 00:57:34 +0300

    java subarray

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,27 @@
-import java.util.HashSet;
 import java.util.Scanner;
-import java.util.Set;
-//https://www.hackerrank.com/challenges/java-hashset/problem?isFullScreen=true
+//https://www.hackerrank.com/challenges/java-negative-subarray/problem?isFullScreen=true
 public class Main {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int t = s.nextInt();
-        s.nextLine();
+        Scanner sc = new Scanner(System.in);
 
-        Set<String>uniquePairs=new HashSet<>();//farklı ciftler icin hashset
+        int n = sc.nextInt(); //dizinin uzunlugu al
+        int[] arr = new int[n];
 
-        for (int i = 0; i < t; i++) {
-            String pair = s.nextLine();
-            uniquePairs.add(pair); //sete ekle
-            System.out.println(uniquePairs.size());// kac tane old. yazdır
+        for (int i = 0; i < n; i++) {//kullanicidan diziyi al
+            arr[i] = sc.nextInt();
         }
-        s.close();
+        int negativeSubarraysCount = 0;
+
+        for (int i = 0; i < n; i++) {
+            int sum =0;
+            for (int j = i; j < n; j++) { //tum alt diziler hessaplanir
+                sum += arr[j];
+
+                if (sum < 0) {
+                    negativeSubarraysCount++;
+                }
+            }
+        }
+        System.out.println(negativeSubarraysCount);//negatif alt dizilerin sayisi
     }
 }
