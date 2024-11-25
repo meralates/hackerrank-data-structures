42fde93ca80cb5a196718158fcdf914238825dbb
Meral ATEŞ <202103001075@dogus.edu.tr>
2024-11-25 18:29:17 +0300

    java array list

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,31 @@
-import java.util.*;
-//https://www.hackerrank.com/challenges/java-generics/problem?isFullScreen=true
-class Printer{
-    public static<T>void printArray(T[]array){
-        for(T element : array){
-            System.out.println(element);
-        }
-    }
-}
-
+import java.util.ArrayList;
+import java.util.Scanner;
+//https://www.hackerrank.com/challenges/java-arraylist/problem?isFullScreen=true
 public class Main {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt(); //array boyutu al
-        Integer[] intArray = new Integer[n];
-        for (int i = 0; i < n; i++) {
-            intArray[i] = scanner.nextInt(); //elemanlari al
+
+        ArrayList<ArrayList<Integer>> mainlist = new ArrayList<>();//array list
+
+        int n = scanner.nextInt(); //satir sayisi
+        for (int i = 0; i < n; i++) { //her satiri okur,arrayliste ekler
+            int d= scanner.nextInt();
+            ArrayList<Integer> row = new ArrayList<>();
+            for (int j = 0; j < d; j++) {
+                row.add(scanner.nextInt());//alt liste
+            }
+            mainlist.add(row);
         }
-        //String array icin input-print
-        int m = scanner.nextInt();
-        String[] stringArray = new String[m];
-        for (int i = 0; i < m; i++) {
-            stringArray[i] = scanner.next();
+        int q = scanner.nextInt(); //sorgu sayısını okur
+        for (int i = 0; i < q; i++) {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            try{
+                System.out.println(mainlist.get(x - 1).get(y - 1));// x satır-y eleman
+            }catch (IndexOutOfBoundsException e){
+                System.out.println("ERROR !!!");
+            }
         }
-        Printer.printArray(intArray);
-        Printer.printArray(stringArray);
-         if (Printer.class.getDeclaredMethods().length>1){
-             System.out.println("The printer class should only have one method named printArray");
-         }
+        scanner.close();
     }
 }
