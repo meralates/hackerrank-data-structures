2f1861a1efc656878f419ff2c930c8971f3799b6
Meral ATEŞ <202103001075@dogus.edu.tr>
2024-11-28 12:26:04 +0300

    java 1D array

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,19 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int testCases = Integer.parseInt(in.nextLine());
+        Scanner scan = new Scanner(System.in);
 
-        List<Student> studentList = new ArrayList<>();
-        while (testCases > 0) {
-            int id= in.nextInt();
-            String name = in.next();
-            double cgpa = in.nextDouble();
+        int n = scan.nextInt();
+        int[] arr = new int[n];
 
-            studentList.add(new Student(id,name,cgpa));
-            testCases--;
+        for(int i = 0; i < n; i++){
+            arr[i] = scan.nextInt();//elemanları diziye alır
         }
-        //sıralama
-        Collections.sort(studentList,new Comparator<Student>(){
-            @Override
-                    public int compare(Student s1,Student s2){
-                if(Double.compare(s2.cgpa,s1.cgpa)!=0){
-                    return Double.compare(s2.cgpa,s1.cgpa);
-                }else if (!s1.name.equals(s2.name)){
-                    return s1.name.compareTo(s2.name);
-                }else{
-                    return Integer.compare(s1.id,s2.id);
-                }
-            }
-        });
-        for(Student st : studentList){
-            System.out.println(st.name);
-        }
-
-    }
-    static class Student {
-        int id;
-        String name;
-        double cgpa; //ort.
-        public Student(int id, String name, double cgpa) {
-            this.id = id;
-            this.name = name;
-            this.cgpa = cgpa;
+        scan.close();
+        //elemanları yazdır
+        for(int i = 0; i < n; i++){
+            System.out.println(arr[i]);
         }
     }
 }
