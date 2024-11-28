253744374708e42a38068b14f296029d262499ba
Meral ATEŞ <202103001075@dogus.edu.tr>
2024-11-28 12:05:13 +0300

    java sort

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,45 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-//https://www.hackerrank.com/challenges/java-list/problem?isFullScreen=true
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
 
-        int n = sc.nextInt();//list uzunluğunu oku
-        ArrayList<Integer> list = new ArrayList<>();
-        //elemanları oku
-        for (int i = 0; i < n; i++) {
-            list.add(sc.nextInt());
+        List<Student> studentList = new ArrayList<>();
+        while (testCases > 0) {
+            int id= in.nextInt();
+            String name = in.next();
+            double cgpa = in.nextDouble();
+
+            studentList.add(new Student(id,name,cgpa));
+            testCases--;
         }
-        //sorgu sayısı
-        int q = sc.nextInt();
-        for (int i = 0; i < q; i++) {
-            String operation = sc.next();//insert veya delete
-            if (operation.equals("Insert")) {
-                int index = sc.nextInt();
-                int element = sc.nextInt();
-                list.add(index, element);
-            }else if (operation.equals("Delete")) {
-                int index = sc.nextInt();
-                list.remove(index);
+        //sıralama
+        Collections.sort(studentList,new Comparator<Student>(){
+            @Override
+                    public int compare(Student s1,Student s2){
+                if(Double.compare(s2.cgpa,s1.cgpa)!=0){
+                    return Double.compare(s2.cgpa,s1.cgpa);
+                }else if (!s1.name.equals(s2.name)){
+                    return s1.name.compareTo(s2.name);
+                }else{
+                    return Integer.compare(s1.id,s2.id);
+                }
             }
+        });
+        for(Student st : studentList){
+            System.out.println(st.name);
         }
-        sc.close();
-        for(int num : list) {
-            System.out.println(num + " ");
+
+    }
+    static class Student {
+        int id;
+        String name;
+        double cgpa; //ort.
+        public Student(int id, String name, double cgpa) {
+            this.id = id;
+            this.name = name;
+            this.cgpa = cgpa;
         }
     }
 }
