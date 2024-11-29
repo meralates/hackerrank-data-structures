ae8411718ff1f7d957b07b304d822841287a87fa
Meral ATEŞ <202103001075@dogus.edu.tr>
2024-11-29 15:39:16 +0300

    java stack

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,38 @@
-import java.util.*;
-//https://www.hackerrank.com/challenges/java-sort/problem?isFullScreen=true
+import java.util.Scanner;
+import java.util.Stack;
+//https://www.hackerrank.com/challenges/java-stack/problem?isFullScreen=true
+//last in first outu kullanır
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int testCases = Integer.parseInt(in.nextLine());
+    public static boolean isBalanced(String str) {
+        Stack<Character> stack = new Stack<>(); // Stack olusur
 
-        List<Student> studentList = new ArrayList<>();
-        while (testCases > 0) {
-            int id= in.nextInt();
-            String name = in.next();
-            double cgpa = in.nextDouble();
-
-            studentList.add(new Student(id,name,cgpa));
-            testCases--;
-        }
-        //sıralama
-        Collections.sort(studentList,new Comparator<Student>(){
-            @Override
-                    public int compare(Student s1,Student s2){
-                if(Double.compare(s2.cgpa,s1.cgpa)!=0){
-                    return Double.compare(s2.cgpa,s1.cgpa);
-                }else if (!s1.name.equals(s2.name)){
-                    return s1.name.compareTo(s2.name);
-                }else{
-                    return Integer.compare(s1.id,s2.id);
+        for (char ch : str.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else if (ch == ')' || ch == '}' || ch == ']') {
+                if (stack.isEmpty()) {//boşsa eşleşme olmaz
+                    return false;
+                }
+                char last = stack.pop();
+                if ((ch == ')' && last != '(') ||
+                        (ch == '}' && last != '{') ||
+                        (ch == ']' && last != '[')) {
+                    return false;
                 }
             }
-        });
-        for(Student st : studentList){
-            System.out.println(st.name);
         }
-
+        return stack.isEmpty();//stack bos olmali
     }
-    static class Student {
-        int id;
-        String name;
-        double cgpa; //ort.
-        public Student(int id, String name, double cgpa) {
-            this.id = id;
-            this.name = name;
-            this.cgpa = cgpa;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String input = scanner.nextLine();
+            System.out.println(isBalanced(input));
         }
+        scanner.close();
     }
 }
+
+
+
+
