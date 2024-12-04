26c202f34b493d502876a961af68bf33cc16ab3f
Meral ATEŞ <202103001075@dogus.edu.tr>
2024-12-04 17:46:58 +0300

    java bitset

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,44 @@
-import java.util.*;
-//https://www.hackerrank.com/challenges/java-dequeue/problem?isFullScreen=true
+import java.util.BitSet;
+import java.util.Scanner;
+//https://www.hackerrank.com/challenges/java-bitset/problem?isFullScreen=true
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();//dizi b.
-        int k = sc.nextInt();//pencere b.
+        int n = sc.nextInt(); //boyutu
+        int m = sc.nextInt(); // islem sayısı
 
-        int[] arr = new int[n];
+        BitSet b1 = new BitSet(n);
+        BitSet b2 = new BitSet(n);
 
-        for (int i = 0; i < n; i++) {//arrayi okur
-            arr[i] = sc.nextInt();
-        }
+        for (int i = 0; i < m; i++) {
+            String operation = sc.next(); // İşlem türü
+            int x = sc.nextInt();
+            int y = sc.nextInt();
 
-        Deque<Integer> deque = new ArrayDeque<>();//alt dizi
-        HashSet<Integer> set  = new HashSet<>();//unique eleman takbi
-        int maxUnique=0;
-
-        for (int i = 0; i < n; i++) {
-            int current=arr[i];
-            deque.add(current);//elemanı ekle
-            set.add(current);//hashsete ekle
-
-            if (deque.size()>k) {//boyutu aştıysa
-                int removed=deque.poll();//ilk elemanı çıkart
-                if (!deque.contains(removed)) {
-                    set.remove(removed);//hashsetten çıkart
-                }
+            switch (operation) {
+                case "AND":
+                    if (x == 1) b1.and(b2);
+                    else b2.and(b1);
+                    break;
+                case "OR":
+                    if (x == 1) b1.or(b2);
+                    else b2.or(b1);
+                    break;
+                case "XOR":
+                    if (x== 1) b1.xor(b2);
+                    else b2.xor(b1);
+                    break;
+                case "FLIP":
+                    if (x == 1) b1.flip(y);
+                    else b2.flip(y);
+                    break;
+                case "SET":
+                    if (x ==1) b1.set(y);
+                    else b2.set(y);
+                    break;
             }
-            maxUnique=Math.max(maxUnique,set.size());
+            System.out.println(b1.cardinality() + " " + b2.cardinality());
         }
-        System.out.println(maxUnique);
+        sc.close();
     }
 }
